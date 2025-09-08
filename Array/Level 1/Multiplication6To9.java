ab832b0df4c853921e94b0c3eff528c52bab99a2
AryanGupta08 <aryanguptaa08@gmail.com>
2025-09-08 23:21:03 +0530

    Initial commit

--- Array/Level 1/Multiplication6To9.java
+++ Array/Level 1/Multiplication6To9.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Multiplication6To9 {
@@ -18,4 +19,26 @@ public class Multiplication6To9 {
     }
 
 
+=======
+import java.util.Scanner;
+
+public class Multiplication6To9 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] a = new int[4];
+        System.out.println("Enter a number: ");
+        int b = input.nextInt();
+        for(int i=0; i<a.length; i++){
+            a[i] = b * (i + 6);
+        }
+
+        for(int i=0; i<a.length; i++){
+            System.out.println(b + " * " + (i + 6) + " = " + a[i]);
+        }
+
+    }
+
+
+>>>>>>> 31d2d66 (Initial commit)
 }
--- Array/Level 1/MultiplicationTable.java
+++ Array/Level 1/MultiplicationTable.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 public class MultiplicationTable {
@@ -18,4 +19,26 @@ public class MultiplicationTable {
     }
 
 
+=======
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] a = new int[10];
+        System.out.println("Enter a number: ");
+        int b = input.nextInt();
+        for(int i=0; i<a.length; i++){
+            a[i] = b * (i + 1);
+        }
+
+        for(int i=0; i<a.length; i++){
+            System.out.println(b + " * " + (i + 1) + " = " + a[i]);
+        }
+
+    }
+
+
+>>>>>>> 31d2d66 (Initial commit)
 }
